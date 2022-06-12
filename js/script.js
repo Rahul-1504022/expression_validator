031992f4cb77b23406aecac64d1fb2bfc19b10ce
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-06-12 13:20:50 +0600

    css style added

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ let cnt = document.getElementById('country_code_input');
 //function
 //show postal code on front-end
 function showPostalCodeForm(e){
-    //let ele = document.getElementById('postal_code_input');
+    phn.style.visibility = "hidden";
+    eml.style.visibility = "hidden";
+    cnt.style.visibility = "hidden";
+    ele.style.visibility = "visible";   
     ele.innerHTML = `<label for="postal_code">Postal Code</label>
                      <input type="number" id="postal_code" name="postal_code">
                      <button type = "submit" value = "submit">Check</button>`
 }
 function clearPostalInput(code){
-    //let ele = document.getElementById('postal_code_input');
     ele.innerHTML = `<label for="postal_code">Postal Code</label>
                      <input type="number" id="postal_code" name="postal_code" value = "${code}">
                      <button type = "submit" value = "submit">Check</button>`
@@ -25,9 +27,11 @@ function inputPostalCode(e){
     let code = document.getElementById('postal_code').value;
     let expression = /^[0-9]{4}$/;
     if(expression.test(code)){
+        result.style.background = "green";
         result.innerHTML = `The input postal code : <b>${code}</b> is valid!`;
     }
     else{
+        result.style.background = "red";
         result.innerHTML = `The input postal code : <b>${code}</b> is invalid!`;
     }
     console.log(code);
@@ -37,6 +41,10 @@ function inputPostalCode(e){
 
 //Phone Number Validator
 function showPhoneForm(){
+    ele.style.visibility = "hidden";
+    eml.style.visibility = "hidden";
+    cnt.style.visibility = "hidden";
+    phn.style.visibility = "visible";
     phn.innerHTML = `<label for="phone">Phone number</label>
     <input type="text" id="phone_no" name="phone">
     <button type = "submit" value = "submit">Check</button>`
@@ -47,9 +55,11 @@ function inputPhonenumber(e){
     let code = document.getElementById('phone_no').value;
     let expression = /^(\+)?(88)?(0(\-)?1)[0-9]{9}$/;
     if(expression.test(code)){
+        result.style.background = "green";
         result.innerHTML = `The input Phone number : <b>${code}</b> is valid!`;
     }
     else{
+        result.style.background = "red";
         result.innerHTML = `The input Phone Number : <b>${code}</b> is invalid!`;
     }
     console.log(code);
@@ -66,6 +76,10 @@ function clearPhoneNumberInput(code){
 
 //email validator
 function showEmailForm(){
+    ele.style.visibility = "hidden";
+    phn.style.visibility = "hidden";
+    cnt.style.visibility = "hidden";
+    eml.style.visibility = "visible"
     eml.innerHTML = `<label for="email_input">Email</label>
     <input type="text" id="email_input" name="email_input">
     <button type = "submit" value = "submit">Check</button>`
@@ -76,9 +90,11 @@ function inputEmail(e){
     let code = document.getElementById('email_input').value;
     let expression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(expression.test(code)){
+        result.style.background = "green";
         result.innerHTML = `The input Email Address : <b>${code}</b> is valid!`;
     }
     else{
+        result.style.background = "red";
         result.innerHTML = `The input Email Address : <b>${code}</b> is invalid!`;
     }
     console.log(code);
@@ -93,6 +109,10 @@ function clearEmailInput(code){
 
 //country code
 function showCountryForm(){
+    ele.style.visibility = "hidden";
+    eml.style.visibility = "hidden";
+    phn.style.visibility = "hidden";
+    cnt.style.visibility = "visible"
     cnt.innerHTML = `<label for="country_input">Country Code</label>
     <input type="text" id="country_input" name="country_input">
     <button type = "submit" value = "submit">Check</button>`
@@ -103,9 +123,11 @@ function inputCountryCode(e){
     let code = document.getElementById('country_input').value;
     let expression = /^(\+?\d{1,3}|\d{1,4})$/;
     if(expression.test(code)){
+        result.style.background = "green";
         result.innerHTML = `The input Country Code : <b>${code}</b> is valid!`;
     }
     else{
+        result.style.background = "red";
         result.innerHTML = `The input Country Code : <b>${code}</b> is invalid!`;
     }
     console.log(code);
